eb62d692fe6660bbc2fa623c2d8bb814f75ac6e8
Øystein Henriksen <83503761+sakspapir@users.noreply.github.com>
2025-07-15 15:47:28 +0200

    Create context.py

---
+++ engine/context.py
@@ -0,0 +1,8 @@
+class Player:
+    def __init__(self):
+        self.inventory = []
+
+class GameContext:
+    def __init__(self, current_room, player):
+        self.current_room = current_room
+        self.player = player
